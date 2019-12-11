5e7204e23d088169cb1649495268cf526b91745b
juhongtao <ju.hongtao@oyohotels.cn>
2019-12-11 17:50:00 +0800

    add guava fix cannot access com.google.common.util.concurrent.AbstractIdleService

--- wallet/src/main/java/com/vector/bitcoinj/explorer/wallet/BitcoinjCastle.java
+++ wallet/src/main/java/com/vector/bitcoinj/explorer/wallet/BitcoinjCastle.java
@@ -1,21 +1,58 @@
 package com.vector.bitcoinj.explorer.wallet;
 
+import org.bitcoinj.core.Address;
 import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.params.RegTestParams;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.utils.BriefLogFormatter;
 
-public class BitcoinjCastle {
+import java.io.File;
 
-    static {
-        NetworkParameters networkParameters = TestNet3Params.get();
-        networkParameters.getId();
-        Coin maxMoneyCoin = networkParameters.getMaxMoney();
-        String maxMoneyStr = maxMoneyCoin.toFriendlyString();
-        System.out.println(" max money " + maxMoneyStr);
-    }
+public class BitcoinjCastle {
 
     public static void main(String[] args) {
         BriefLogFormatter.init();
+        if (args.length < 2) {
+            System.err.println("Usage: address-to-send-back-to [regtest|testnet]");
+            return;
+        }
+        // Figure out which network we should connect to. Each one gets its own set of files.
+        NetworkParameters params;
+        String filePrefix;
+        if (args[1].equals("testnet")) {
+            params = TestNet3Params.get();
+            filePrefix = "forwarding-service-testnet";
+        } else if (args[1].equals("regtest")) {
+            params = RegTestParams.get();
+            filePrefix = "forwarding-service-regtest";
+        } else {
+            params = MainNetParams.get();
+            filePrefix = "forwarding-service";
+        }
+
+        WalletAppKit walletAppKit = new WalletAppKit(params, new File("."), filePrefix) {
+            @Override
+            protected void onSetupCompleted() {
+                // This is called in a background thread after startAndWait is called, as setting up various objects
+                // can do disk and network IO that may cause UI jank/stuttering in wallet apps if it were to be done
+                // on the main thread.
+                if (wallet().getKeyChainGroupSize() < 1)
+                    wallet().importKey(new ECKey());
+            }
+        };
+
+        if (params == RegTestParams.get()) {
+            // Regression test mode is designed for testing and development only, so there's no public network for it.
+            // If you pick this mode, you're expected to be running a local "bitcoind -regtest" instance.
+            walletAppKit.connectToLocalHost();
+        }
+
+        // Download the block chain and wait until it's done.
+        walletAppKit.startAsync();
+        walletAppKit.awaitRunning();
     }
 }
